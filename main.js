b0be5fd09f9dd9eff9608462d8ef76b38ce48da6
Atis89 <fejerattila89@gmail.com>
2020-02-28 10:14:43 +0100

    array even number

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-const arr = (n) => {
+
 n = Math.floor(Math.random() * 10 + 1);
 array = Array(n).fill(0);
 console.log(array);
+
+
+
+const fgv = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      
+    }
+    console.log(arr[i]);
+  }
+
 }
-
-
-
-
