990006da6c20ba5e4f0ca1a61e5c896016c350cd
SutyaM <sutya.miklos.cv@gmail.com>
2020-02-28 10:11:35 +0100

    2., 4., 6., 8., 10. feladat

---
+++ main.js
@@ -0,0 +1,60 @@
+// const arr = [0, 0, 0, 0, 0, 0];
+
+const fillRandom = (a) => {
+  for (let i = 0; i < a.length; i++) {
+    a[i] = Math.floor(Math.random() * 1000)
+  }
+  return a;
+};
+
+// console.log(fillRandom(arr));
+
+const indexOfOdd = (a) => {
+  const indexes = [];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] % 2 !== 0) {
+      indexes.push(i)
+    }
+  }
+  return indexes;
+}
+
+// console.log(indexOfOdd(arr));
+
+const numberOfEvenIndexes = (a) => {
+  let number = 0;
+  for (let i = 0; i < a.length; i++) {
+    if(a[i] % 2 === 0 && i % 2 !== 0) {
+        number++;
+    }
+  }
+  return number;
+};
+
+// console.log(numberOfEvenIndexes(arr));
+
+const meanOfElements = (a) => {
+  let sum = 0;
+  for (let i = 0; i < a.length; i++) {
+    sum += a[i];
+  }
+  const mean = sum / a.length;
+  return mean;
+}
+
+// const arr2 = [5, 6, 7, 2, 9, 1];
+
+// console.log(meanOfElements(arr));
+
+const smallestElement = (a) => {
+  let small = a[0]
+  for (let i = 1; i < a.length; i++) {
+    if(a[i] < small) {
+      small = a[i];
+    }
+  }
+  return small;
+};
+
+// console.log(smallestElement(arr2));
+
